49f2cab0e52aaf6cc57f7520e9728f6043803367
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-07-03 08:22:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,70 +6,61 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 Bot = Client(
-    "Corona-Info-Bot",
+    "Torrent-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
-API = "https://api.sumanjay.cf/covid/?country="
-
-START_TEXT = """Hello {},
-I am a simple corona information of a country telegram bot.
-
-Made by @FayasNoushad"""
-
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')]])
-
+API = "https://api.safone.tech/torrent?query="
 
 @Bot.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    await update.reply_text(
-        text=START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=BUTTONS,
-        quote=True
+async def start(bot, message):
+    await message.reply(
+        text="Hello I'am a simple Torrent finder Bot."
     )
 
 
 @Bot.on_message(filters.private & filters.text)
-async def reply_info(bot, update):
-    reply_markup = BUTTONS
-    await update.reply_text(
-        text=covid_info(update.text),
-        disable_web_page_preview=True,
-        quote=True,
-        reply_markup=reply_markup
+async def reply_info(bot, message):
+    await message.reply(
+        text=torrent(message.text)
     )
 
 
-def covid_info(country_name):
+def torrent(query):
     try:
-        r = requests.get(API + requote_uri(country_name.lower()))
+        r = requests.get(API + requote_uri(query.lower()))
         info = r.json()
-        country = info['country'].capitalize()
-        active = info['active']
-        confirmed = info['confirmed']
-        deaths = info['deaths']
-        info_id = info['id']
-        last_update = info['last_update']
-        latitude = info['latitude']
-        longitude = info['longitude']
-        recovered = info['recovered']
-        covid_info = f"""--**Covid 19 Information**--
+        name = info['name']
+        category = info['category']
+        description = info['description']
+        genre = info['genre']
+        infoHash = info['infoHash']
+        language = info['language']
+        lastChecked = info['lastChecked']
+        seeders = info['seeders']
+        leechers = info['leechers']
+        size = info['size']
+        uploaderLink = info['uploaderLink']
+        uploadDate = info['uploadDate']
+        magnetLink = magnetLink['longitude']
+        torrent = f"""--**Torrent Information**--
 
-Country : `{country}`
-Actived : `{active}`
-Confirmed : `{confirmed}`
-Deaths : `{deaths}`
-ID : `{info_id}`
-Last Update : `{last_update}`
-Latitude : `{latitude}`
-Longitude : `{longitude}`
-Recovered : `{recovered}`
-
-Made by @FayasNoushad"""
-        return covid_info
+Name : `{name}`
+Category : `{category}`
+Description : `{description}`
+Genre : `{genre}`
+Info HASH : `{infoHash}`
+Language : `{language}`
+Size : `{size}`
+Last Checked : `{lastChecked}`
+Seeders : `{seeders}`
+Leechers : `{leechers}`
+Uploader Link : `{uploaderLink}`
+Upload Date : `{uploadDate}`
+Magnet Link : `{magnetLink}`"""
+        return torrent
     except Exception as error:
         return error
 
